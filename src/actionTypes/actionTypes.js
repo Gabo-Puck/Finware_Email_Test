6c5df11430f57363ffd21cf3ccfff4f68485cbfb
Gabo-Puck <melkorgodo@gmail.com>
2023-04-21 01:40:53 -0500

    Añadido primer idea para actionTypes de la aplicacion

---
+++ src/actionTypes/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_EMAIL = "ADD_EMAIL";
+export const UNREAD_EMAIL = "UNREAD_EMAIL";
+export const DELETE_EMAIL = "DELETE_EMAIL";
+export const SELECT_EMAIL = "SELECT_EMAIL";
+export const SPAM_EMAIL = "SPAM_EMAIL";
+export const CHANGE_FILTER = "CHANGE_FILTER";
