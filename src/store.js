6d33564ecbfd8e5d0d502aaade176ff768bea900
Gabo-Puck <melkorgodo@gmail.com>
2023-04-22 13:50:07 -0500

    Añadido middleware para obtener emails del servidor"

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
 import { createStore } from "redux";
 import rootReducer from "./reducer";
+import { applyMiddleware } from "redux";
+import stateLogger from "./features/stateLoggerMiddleware";
+import fetchEmails from "./features/fetchEmailsMiddleware";
 
-const store = createStore(rootReducer);
+const enchancedMiddleware = applyMiddleware(stateLogger, fetchEmails);
+
+const store = createStore(rootReducer, enchancedMiddleware);
 
 export default store;
