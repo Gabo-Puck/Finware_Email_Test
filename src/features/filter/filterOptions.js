cf28a8ca9b65b784e8e3775d04087b3744f56c5f
Gabo-Puck <melkorgodo@gmail.com>
2023-04-22 15:30:36 -0500

    Añadido opciones para filtro de lista de emails (Inbox, Spam y Borrar/Delete)

---
+++ src/features/filter/filterOptions.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { CHANGE_FILTER } from "../../actionTypes/actionTypes";
+import { useDispatch } from "react-redux/es/exports";
+
+const FilterOptions = () => {
+  const dispatch = useDispatch();
+
+  const handleClickDelete = () => {
+    dispatch({ type: CHANGE_FILTER, payload: "deleted" });
+  };
+  const handleClickSpam = () => {
+    dispatch({ type: CHANGE_FILTER, payload: "spam" });
+  };
+  const handleClickInbox = () => {
+    dispatch({ type: CHANGE_FILTER, payload: "inbox" });
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickDelete}>Delete</button>
+      <button onClick={handleClickSpam}>Spam</button>
+      <button onClick={handleClickInbox}>Inbox</button>
+    </div>
+  );
+};
+
+export default FilterOptions;
